43bc75ee494e6f981d5a9781f2768e07b0347a55
pawelru <pawel.rulka@infotech.edu.pl>
2026-03-18 10:04:20 +0100

    - Dodano widoki: lista linii, szczegóły trasy linii - Zaktualizowano urls.py i views.py o powyższe - Ulepszono widok szczegółów kursu w city_detail.html - Zmieniono widok znaczników z trip_short_name - Inne pomniejsze poprawki i ulepszenia

--- CzyPrzyjedzieApp/views.py
+++ CzyPrzyjedzieApp/views.py
@@ -17,12 +17,49 @@ def city_detail(request, city_slug, vehicle_id=None, feed_name=None, trip_id=Non
     city = get_object_or_404(City, name=city_slug)
     context = {
         'city': city,
+        'initial_view': 'map',
+        'initial_route_feed_name': None,
+        'initial_route_id': None,
         'selected_vehicle_id': vehicle_id,
         'has_vehicle_view': bool(vehicle_id),
         'selected_feed_name': feed_name,
         'selected_trip_id': trip_id,
         'selected_date': date,
         'has_trip_view': bool(trip_id),
+    }
+    return render(request, 'city_detail.html', context)
+
+
+def routes_list(request, city_slug):
+    city = get_object_or_404(City, name=city_slug)
+    context = {
+        'city': city,
+        'initial_view': 'routes_list',
+        'initial_route_feed_name': None,
+        'initial_route_id': None,
+        'selected_vehicle_id': None,
+        'has_vehicle_view': False,
+        'selected_feed_name': '',
+        'selected_trip_id': '',
+        'selected_date': '',
+        'has_trip_view': False,
+    }
+    return render(request, 'city_detail.html', context)
+
+
+def route_detail(request, city_slug, feed_name, route_id):
+    city = get_object_or_404(City, name=city_slug)
+    context = {
+        'city': city,
+        'initial_view': 'route_detail',
+        'initial_route_feed_name': feed_name,
+        'initial_route_id': route_id,
+        'selected_vehicle_id': None,
+        'has_vehicle_view': False,
+        'selected_feed_name': '',
+        'selected_trip_id': '',
+        'selected_date': '',
+        'has_trip_view': False,
     }
     return render(request, 'city_detail.html', context)
 
