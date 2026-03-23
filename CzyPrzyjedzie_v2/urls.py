157ea49a6a5ddb2d6ade7c446d58013a2057475a
pawelru <pawel.rulka@infotech.edu.pl>
2026-03-23 14:34:22 +0100

    Widok brygady część 2: - Możliwość podglądu rozkładu brygady per pojazd - Obsługa tej funkcji w city_detail.html i urls.py
    - Dodanie okienka wywoływanego kliknięciem w widoku kursu / pojazdu
    - Okienko zawiera przyciski: Udostępnij, Rozkład brygady, Informacje o pojeździe
    - Przycisków może być mniej jeżeli nie mamy odpowiednich danych
    
    - Poprawki w generowaniu danych GTFS i GTFS-Realtime dla Białegostoku
    - Uwzględnienie tych poprawek w bazie danych

--- CzyPrzyjedzie_v2/urls.py
+++ CzyPrzyjedzie_v2/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('<slug:city_slug>/routes/<str:feed_name>/<path:route_id>/', views.route_detail, name='city_route_detail'),
     path('<slug:city_slug>/routes/<str:feed_name>/<path:route_id>/brigades/', views.route_brigades, name='city_route_brigades'),
     path('<slug:city_slug>/routes/<str:feed_name>/<path:route_id>/brigades/<str:block_id>/', views.brigade_detail, name='city_brigade_detail'),
+    # Widok brygady per pojazd / kurs (block_id wykrywany automatycznie w JS)
+    # UWAGA: te route'y muszą być PRZED route'm dla pojazdu, bo <path:vehicle_id>
+    # mogłoby "pożreć" segment '/brigade'.
+    path('<slug:city_slug>/vehicle/<path:vehicle_id>/brigade/', views.city_detail, name='city_vehicle_brigade_detail'),
+    path('<slug:city_slug>/trip/<str:feed_name>/<str:trip_id>/<str:date>/brigade/', views.city_detail, name='city_trip_brigade_detail'),
     path('<slug:city_slug>/vehicle/<path:vehicle_id>/', views.city_detail, name='city_vehicle_detail'),
     path('<slug:city_slug>/trip/<str:feed_name>/<str:trip_id>/<str:date>/', views.city_detail, name='city_trip_detail'),
     path('<slug:city_slug>/', views.city_detail, name='city_detail'),
